82a88561017a85575afae9a83cda7e80c64aa2df
sreesaiogetiammini <111784318+sreesaiogetiammini@users.noreply.github.com>
2022-09-23 20:24:03 -0600

    Update drawHelpers.hpp

--- src/drawHelpers.hpp
+++ src/drawHelpers.hpp
@@ -17,25 +17,26 @@ struct Position {
 
 bool checkMousePosition(sf::RenderWindow& window, sf::Shape& text);
 void drawBackground(sf::RenderWindow& window);
-sf::Text* drawText(const std::string& text,const sf::Vector2f& textPosition,const sf::Color& textColor ,const size_t& characterSize);
-sf::Text* drawGameTitle();
-sf::Text* drawPlayerText();
-sf::Text* drawDelearText();
-sf::Text* drawPlayText();
-sf::Text* drawQuitText();
-sf::RectangleShape* drawRectangle(const sf::Vector2f& rectanglePosition, const sf::Vector2f& rectangularSize, const sf::Color& rectangleFillColor);
-sf::RectangleShape* drawPlayRect();
-sf::RectangleShape* drawQuitRect();
-sf::Text* drawHitText();
-sf::Text* drawStandText();
-sf::Text* drawCardText(const Card card ,sf::Vector2f& cardPosition);
-sf::RectangleShape* drawCardRec(sf::Vector2f& cardPos, bool visible);
-sf::CircleShape* drawHitCircle();
-sf::CircleShape* drawStandCircle();
-sf::CircleShape* drawCircle(const float& radius, const sf::Vector2f& circlePosition, const sf::Color& circleFillColor);
-sf::Text* drawPlayerScore(unsigned short score);
-sf::CircleShape* drawPlayerScoreCircle();
-sf::Text* drawDealerScore(unsigned short score);
-sf::CircleShape* drawDealerScoreCircle();
+
+std::unique_ptr<sf::Text> drawText(const std::string& text,const sf::Vector2f& textPosition,const sf::Color& textColor ,const size_t& characterSize);
+std::unique_ptr<sf::Text> drawGameTitle();
+std::unique_ptr<sf::Text> drawPlayerText();
+std::unique_ptr<sf::Text> drawDelearText();
+std::unique_ptr<sf::Text> drawPlayText();
+std::unique_ptr<sf::Text> drawQuitText();
+std::unique_ptr<sf::RectangleShape> drawRectangle(const sf::Vector2f& rectanglePosition, const sf::Vector2f& rectangularSize, const sf::Color& rectangleFillColor);
+std::unique_ptr<sf::RectangleShape> drawPlayRect();
+std::unique_ptr<sf::RectangleShape> drawQuitRect();
+std::unique_ptr<sf::Text> drawHitText();
+std::unique_ptr<sf::Text> drawStandText();
+std::unique_ptr<sf::Text> drawCardText(const Card card ,sf::Vector2f& cardPosition);
+std::unique_ptr<sf::RectangleShape> drawCardRec(sf::Vector2f& cardPos, bool visible);
+std::unique_ptr<sf::CircleShape> drawHitCircle();
+std::unique_ptr<sf::CircleShape> drawStandCircle();
+std::unique_ptr<sf::CircleShape> drawCircle(const float& radius, const sf::Vector2f& circlePosition, const sf::Color& circleFillColor);
+std::unique_ptr<sf::Text> drawPlayerScore(unsigned short score);
+std::unique_ptr<sf::CircleShape> drawPlayerScoreCircle();
+std::unique_ptr<sf::Text> drawDealerScore(unsigned short score);
+std::unique_ptr<sf::CircleShape> drawDealerScoreCircle();
 
 #endif /* drawHelpers_hpp */
